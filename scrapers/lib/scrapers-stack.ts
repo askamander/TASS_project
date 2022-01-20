35b0bc7ba96cb03e4edfc564ef7bb4bd381191da
Artur Grudkowski <artur.grudkowski@atende.industries>
2022-01-20 13:52:07 +0100

    Remove unused bucket

--- scrapers/lib/scrapers-stack.ts
+++ scrapers/lib/scrapers-stack.ts
@@ -1,6 +1,5 @@
 import * as path from 'path';
 
-import * as s3 from '@aws-cdk/aws-s3';
 import * as cdk from '@aws-cdk/core';
 import * as iam from '@aws-cdk/aws-iam';
 import * as lambda from '@aws-cdk/aws-lambda';
@@ -9,8 +8,6 @@ import * as dynamodb from '@aws-cdk/aws-dynamodb';
 export class ScrapersStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    const bucket = new s3.Bucket(this, 'TASSDataStorage');
 
     const tassResultsTable = new dynamodb.Table(this, 'TASSResults', {
       partitionKey: { name: 'book_id', type: dynamodb.AttributeType.NUMBER },
